478cf9e68a869c3ef5a9291cf4b0e695ba0d8212
rouxiaoxiao <rouxiaoxiao222@gmail.com>
2019-12-06 10:46:23 +0800

    int

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+    </div>
+</template>
+<style>
+    @import "./assets/css/main.css";
+    /*这里的css主要控制顶部栏等颜色样式*/
+    /*@import "./assets/css/color-dark.css";     !*深色主题*!*/
+    @import "./assets/css/color-gugong.css";     /*草绿主题*/
+    /*@import "./assets/css/theme-green/color-green.css";   !*浅绿色主题*!*/
+</style>
